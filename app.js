3bc4915040dc344bbf798ec66208b696e9d64547
DasariTejaReddy <tejareddydasari@gmail.com>
2023-04-05 21:16:23 -0500

    added new files

--- app.js
+++ app.js
@@ -6,8 +6,11 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var dogsRouter = require('./routes/Dogs');
+var boardRouter = require('./routes/board');
+//var selectorRouter = require('./routes/selector');
 
-var app = express();
+//var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,6 +24,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/Dogs', dogsRouter);
+app.use('/board', boardRouter);
+app.use('/selector', selectorRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
